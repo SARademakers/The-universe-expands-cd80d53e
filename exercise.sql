b58e77fc2bd4e3d117ef6e455626d24d9c629902
SARademakers <2049137@talnet.nl>
2020-02-04 11:57:01 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,4 @@
+USE Sterrenstelsel;
+INSERT INTO planeten VALUES ('Pluto'), ('Aarde'), ('Neptunes'), ('Jupiter'), ('Mars'), ('Mercurius'), ('Uranus'), ('Venus'), ('Saturnus');
+SHOW TABLES;
+DESCRIBE planeten;
